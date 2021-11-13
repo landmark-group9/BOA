ef4a51b18745402e3d427366e7e05a22772fcaf9
Cloud User <ceros931@outlook.com>
2021-11-13 12:58:36 +0000

    dbserver management file

---
+++ dbserver.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo function starts
+
+coldfn()
+{
+
+echo incerting parameters
+echo parameter incerted
+echo ejecting disk
+echo disk ejected
+
+echo wrong option passed
+echo pass insert or eject
+}
+coldfn
+
+echo function  ends
